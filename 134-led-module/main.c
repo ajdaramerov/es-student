2ed4277ae92a1b065f0db80199267e06a7885329
aidar <ajdaramerov@gmail.com>
2026-09-16 19:15:08 +0300

    Сделана п1.3.5

---
+++ 134-led-module/main.c
@@ -0,0 +1,68 @@
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include <stdio.h>
+#include "led.h"
+
+const uint BUTTON_PIN = 24;
+bool previous = true;  
+bool led = true;
+
+const uint DEBOUNCE_MS = 20;
+
+bool get_button_debounce(uint pin)
+{
+    bool state = gpio_get(pin);
+    sleep_ms(DEBOUNCE_MS);
+    return state && gpio_get(pin);
+}
+
+
+void handle_command(int command)
+{
+    if (command == 'e')
+    {
+        led_set(true);
+        printf("led on\n");
+    }
+    else if (command == 'd')
+    {
+        led_set(false);
+        printf("led off\n");
+    }
+    else
+    {
+        printf("unknown command: %c\n", command);
+    }
+
+}
+
+int main()
+{
+    stdio_init_all();
+    led_init();
+    gpio_init(BUTTON_PIN);
+    gpio_set_dir(BUTTON_PIN, GPIO_IN);
+    gpio_pull_up(BUTTON_PIN);
+
+    while (1)
+    {
+        bool current = get_button_debounce(BUTTON_PIN);
+
+        if (previous == true && current == false)
+        {
+            led_toggle();
+            printf("led %s\n", led_is_on() ? "on" : "off");
+        }
+
+        previous = current;
+
+        int command = getchar_timeout_us(0);
+
+        if (command == PICO_ERROR_TIMEOUT)
+        {
+            continue;
+        }
+
+        handle_command(command);
+    }
+}
