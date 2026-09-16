138d1bc5fead0b47ef8f015a36fb74c93428ed84
aidar <ajdaramerov@gmail.com>
2026-09-16 18:25:42 +0300

    Сделана п1.3.4

--- 133-led-button-usb/main.c
+++ 133-led-button-usb/main.c
@@ -24,6 +24,26 @@ void set_led(bool on)
     printf("led %s\n", on ? "on" : "off");
 }
 
+bool handle_command(int command, bool led)
+{
+    if (command == 'e')
+    {
+        led = true;
+        set_led(led);
+    }
+    else if (command == 'd')
+    {
+        led = false;
+        set_led(led);
+    }
+    else
+    {
+        printf("unknown command: %c\n", command);
+    }
+
+    return led;
+}
+
 int main()
 {
     stdio_init_all();
@@ -39,7 +59,7 @@ int main()
 
     while (1)
     {
-                bool current = get_button_debounce(BUTTON_PIN);
+        bool current = get_button_debounce(BUTTON_PIN);
 
         if (previous == true && current == false)
         {
@@ -48,5 +68,14 @@ int main()
         }
 
         previous = current;
+
+        int command = getchar_timeout_us(0);
+
+        if (command == PICO_ERROR_TIMEOUT)
+        {
+            continue;
+        }
+
+        led = handle_command(command, led);
     }
 }
